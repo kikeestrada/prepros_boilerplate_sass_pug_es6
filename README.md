c3ac11ca76f94398339229e80d55c1ee11e21c8e
kikeestrada <kikeestrada@gmail.com>
2020-04-17 14:12:28 -0600

    first load

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prepros boilerplate scaffolding
+* ```sass```
+* ```pug```
+* ```es6-beautify```
+
+
+gh pages link: https://kikeestrada.github.io/prepros_boilerplate_sass_pug_es6
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import {hello, bye} from "./components/example"
+
+(()=>{
+	hello();
+	bye();
+	if (document.body.classList.contains('home')) {
+		// functions here
+	}else if (document.body.classList.contains('page2')) {
+		// functions here
+	}else if (document.body.classList.contains('page3')) {
+		// functions here
+	}
+})();
